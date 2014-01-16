b2fa6e2d7e26f02f7b9d077bbe671aed31c5a13c
David White <david@monkeyphp.com>
2014-01-16 22:29:02 +0000

    Added comment to LockException

--- src/OpenWeatherMap/Exception/LockException.php
+++ src/OpenWeatherMap/Exception/LockException.php
@@ -1,18 +1,38 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * LockException.php
+ * 
+ * @category   OpenWeatherMap
+ * @package    OpenWeatherMap
+ * @subpackage OpenWeatherMap\Exception
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ * 
+ * Copyright (C) 2014  David White
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
 namespace OpenWeatherMap\Exception;
 
 use Exception;
 
 /**
- * Description of LockException
- *
- * @author David White <david@monkeyphp.com>
+ * LockException
+ * 
+ * @category   OpenWeatherMap
+ * @package    OpenWeatherMap
+ * @subpackage OpenWeatherMap\Exception
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
  */
 class LockException extends Exception
 {
