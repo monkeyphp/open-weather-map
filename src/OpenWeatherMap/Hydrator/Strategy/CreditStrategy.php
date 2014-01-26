73d57994f6ce2478233981e06987d859cd7d1b09
David White <david@monkeyphp.com>
2014-01-26 01:25:50 +0000

    Woops

--- src/OpenWeatherMap/Hydrator/Strategy/CreditStrategy.php
+++ src/OpenWeatherMap/Hydrator/Strategy/CreditStrategy.php
@@ -1,11 +1,27 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * CreditStrategy.php
+ * 
+ * @category   OpenWeatherMap
+ * @package    OpenWeatherMap
+ * @subpackage OpenWeatherMap\Hydrator\Strategy
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ * 
+ * Copyright (C) 2014  David White
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
-
 namespace OpenWeatherMap\Hydrator\Strategy;
 
 use OpenWeatherMap\Entity\Credit;
@@ -13,14 +29,27 @@ use Zend\Stdlib\Hydrator\ClassMethods;
 use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
 
 /**
- * Description of CreditStrategy
- *
- * @author David White <david@monkeyphp.com>
+ * CreditStrategy
+ * 
+ * @category   OpenWeatherMap
+ * @package    OpenWeatherMap
+ * @subpackage OpenWeatherMap\Hydrator\Strategy
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
  */
 class CreditStrategy implements StrategyInterface
 {
+    /**
+     * Instance of ClassMethods hydrator
+     * 
+     * @var ClassMethods
+     */
     protected $hydrator;
     
+    /**
+     * Return an instance of ClassMethods hydrator
+     * 
+     * @return ClassMethods
+     */
     protected function getHydrator()
     {
         if (! isset($this->hydrator)) {
@@ -29,14 +58,30 @@ class CreditStrategy implements StrategyInterface
         return $this->hydrator;
     }
     
+    /**
+     * Extract the values from an instance of Credit
+     * 
+     * @param Credit $value
+     * 
+     * @return null|array
+     */
     public function extract($value)
     {
-        
+        if (! $value instanceof Credit) {
+            return null;
+        }
+        return $this->getHydrator()->extract($value);
     }
-
+    
+    /**
+     * Hydrate and return an instance of Credit using the supplied array of values
+     * 
+     * @param array $value
+     * 
+     * @return Credit
+     */
     public function hydrate($value)
     {
         return $this->getHydrator()->hydrate($value, new Credit());
     }
-
 }
