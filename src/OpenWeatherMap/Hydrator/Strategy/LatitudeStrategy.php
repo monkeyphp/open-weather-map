fedfdc72a5d1c165fc2f6bf93583bafec67ed6d0
David White <david@monkeyphp.com>
2014-01-26 00:47:49 +0000

    Added docblock to LatitudeStrategy

--- src/OpenWeatherMap/Hydrator/Strategy/LatitudeStrategy.php
+++ src/OpenWeatherMap/Hydrator/Strategy/LatitudeStrategy.php
@@ -1,17 +1,40 @@
 <?php
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * LatitudeStrategy.php
+ * 
+ * @category   OpenWeatherMap
+ * @package    OpenWeatherMap
+ * @subpackage OpenWeatherMap\Hydrator\Strategy
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ * 
+ * Copyright (C) 2014  David White
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
 namespace OpenWeatherMap\Hydrator\Strategy;
+
+use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
+
 /**
- * Description of LatitudeStrategy
- *
- * @author David White <david@monkeyphp.com>
+ * LatitudeStrategy
+ * 
+ * @category   OpenWeatherMap
+ * @package    OpenWeatherMap
+ * @subpackage OpenWeatherMap\Hydrator\Strategy
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
  */
-class LongitudeStrategy implements \Zend\Stdlib\Hydrator\Strategy\StrategyInterface
+class LatitudeStrategy implements StrategyInterface
 {
     
     public function extract($value)
@@ -20,9 +43,7 @@ class LongitudeStrategy implements \Zend\Stdlib\Hydrator\Strategy\StrategyInterf
     }
 
     public function hydrate($value)
-    {   
-        var_dump(__LINE__);
-        var_dump($value);
+    {
+        
     }
-
 }
