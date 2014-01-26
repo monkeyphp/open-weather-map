857f954385e7a9e8a89506d4881112aab15b0dbd
David White <david@monkeyphp.com>
2014-01-26 21:40:46 +0000

    Added tests for Weather

---
+++ tests/OpenWeatherMapTest/Entity/WeatherTest.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * WeatherTest.php
+ * 
+ * @category   OpenWeatherMapTest
+ * @package    OpenWeatherMapTest
+ * @subpackage OpenWeatherMapTest\Entity
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ * 
+ * Copyright (C) 2014  David White
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ */
+namespace OpenWeatherMapTest\Entity;
+
+use OpenWeatherMap\Entity\Weather;
+use PHPUnit_Framework_TestCase;
+
+/**
+ * WeatherTest
+ * 
+ * @category   OpenWeatherMapTest
+ * @package    OpenWeatherMapTest
+ * @subpackage OpenWeatherMapTest\Entity
+ * @author     David White [monkeyphp] <david@monkeyphp.com>
+ */
+class WeatherTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetSetNumber()
+    {
+        $number = "802";
+        $weather = new Weather();
+        
+        $this->assertNull($weather->getNumber());
+        $this->assertSame($weather, $weather->setNumber($number));
+        $this->assertEquals($number, $weather->getNumber());
+    }
+    
+    public function testGetSetValue()
+    {
+        $value = "scattered clouds";
+        $weather = new Weather();
+        
+        $this->assertNull($weather->getValue());
+        $this->assertSame($weather, $weather->setValue($value));
+        $this->assertEquals($value, $weather->getValue());
+    }
+    
+    public function testGetSetIcon()
+    {
+        $icon = "03n";
+        $weather = new Weather();
+        
+        $this->assertNull($weather->getIcon());
+        $this->assertSame($weather, $weather->setIcon($icon));
+        $this->assertEquals($icon, $weather->getIcon());
+    }
+}
