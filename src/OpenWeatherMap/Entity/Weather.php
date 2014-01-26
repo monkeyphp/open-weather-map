6050e13b3330e4948662dff3125fb28ffb23f6f7
David White <david@monkeyphp.com>
2014-01-26 21:34:26 +0000

    Added comments to Weather

--- src/OpenWeatherMap/Entity/Weather.php
+++ src/OpenWeatherMap/Entity/Weather.php
@@ -24,6 +24,8 @@
  */
 namespace OpenWeatherMap\Entity;
 
+use InvalidArgumentException;
+
 /**
  * Weather
  * 
@@ -34,41 +36,110 @@ namespace OpenWeatherMap\Entity;
  */
 class Weather
 {
+    /**
+     * The number value of the Weather
+     * 
+     * @var string|null
+     */
     protected $number;
     
+    /**
+     * The value of the Weather
+     * 
+     * @var string|null
+     */
     protected $value;
     
+    /**
+     * The icon value
+     * 
+     * @var string|null
+     */
     protected $icon;
     
+    /**
+     * Return the number of the Weather
+     * 
+     * @return string|null
+     */
     public function getNumber()
     {
         return $this->number;
     }
-
+    
+    /**
+     * Return the value of the Weather
+     * 
+     * @return string|null
+     */
     public function getValue()
     {
         return $this->value;
     }
 
+    /**
+     * Return the icon value of the Weather
+     * 
+     * @return string|null
+     */
     public function getIcon()
     {
         return $this->icon;
     }
 
-    public function setNumber($number)
+    /**
+     * Set the number value of the Weather
+     * 
+     * @param string|null $number
+     * 
+     * @return Weather
+     * @throws InvalidArgumentException
+     */
+    public function setNumber($number = null)
     {
+        if (! is_null($number)) {
+            if (! is_string($number)) {
+                throw new InvalidArgumentException('Expects a string');
+            }
+        }
         $this->number = $number;
         return $this;
     }
-
-    public function setValue($value)
+    
+    /**
+     * Set the value of the Weather
+     * 
+     * @param string|null $value
+     * 
+     * @return Weather
+     * @throws InvalidArgumentException
+     */
+    public function setValue($value = null)
     {
+        if (! is_null($value)) {
+            if (! is_string($value)) {
+                throw new InvalidArgumentException('Expects a string');
+            }
+        }
         $this->value = $value;
         return $this;
     }
 
-    public function setIcon($icon)
+    /**
+     * Set the icon value
+     * 
+     * @param string|null $icon
+     * 
+     * @return Weather
+     * @throws InvalidArgumentException
+     */
+    public function setIcon($icon = null)
     {
+        if (! is_null($icon)) {
+            if (! is_string($icon)) {
+                throw new InvalidArgumentException('Expects a string');
+            }
+        }
         $this->icon = $icon;
         return $this;
     }
