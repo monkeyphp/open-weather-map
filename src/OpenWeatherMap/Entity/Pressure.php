cda501ebb750058ba11504ecea41531818aa3049
David White <david@monkeyphp.com>
2014-01-28 20:16:49 +0000

    Created tests for Pressure

--- src/OpenWeatherMap/Entity/Pressure.php
+++ src/OpenWeatherMap/Entity/Pressure.php
@@ -1,32 +1,34 @@
 <?php
 /**
  * Pressure.php
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Entity
  * @author     David White [monkeyphp] <david@monkeyphp.com>
- * 
+ *
  * Copyright (C) 2014  David White
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
 namespace OpenWeatherMap\Entity;
 
+use InvalidArgumentException;
+
 /**
  * Pressure
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Entity
@@ -34,28 +36,70 @@ namespace OpenWeatherMap\Entity;
  */
 class Pressure
 {
+    /**
+     *
+     * @var string|null
+     */
     protected $value;
-    
+
+    /**
+     *
+     * @var string|null
+     */
     protected $unit;
-    
+
+    /**
+     *
+     * @return string|null
+     */
     public function getValue()
     {
         return $this->value;
     }
 
+    /**
+     *
+     * @return string|null
+     */
     public function getUnit()
     {
         return $this->unit;
     }
 
-    public function setValue($value)
+    /**
+     * Set the value of the pressure
+     *
+     * @param string|null $value
+     *
+     * @return Pressure
+     * @throws InvalidArgumentException
+     */
+    public function setValue($value = null)
     {
+        if (! is_null($value)) {
+            if (! is_string($value)) {
+                throw new InvalidArgumentException('Expects a string');
+            }
+        }
         $this->value = $value;
         return $this;
     }
 
-    public function setUnit($unit)
+    /**
+     * Set the unit value
+     *
+     * @param string|null $unit
+     *
+     * @return Pressure
+     * @throws InvalidArgumentException
+     */
+    public function setUnit($unit = null)
     {
+        if (! is_null($unit)) {
+            if (! is_string($unit)) {
+                throw new InvalidArgumentException('Expects a string');
+            }
+        }
         $this->unit = $unit;
         return $this;
     }
