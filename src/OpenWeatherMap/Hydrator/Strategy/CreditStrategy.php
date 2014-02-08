0b7f620005da74aa3dbdab6fb54e6cacb59136af
David White <david@monkeyphp.com>
2014-02-08 12:42:23 +0000

    Updated tests for CreditStrategy

--- src/OpenWeatherMap/Hydrator/Strategy/CreditStrategy.php
+++ src/OpenWeatherMap/Hydrator/Strategy/CreditStrategy.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * CreditStrategy.php
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Hydrator\Strategy
  * @author     David White [monkeyphp] <david@monkeyphp.com>
- * 
+ *
  * Copyright (C) 2014  David White
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
@@ -30,7 +30,7 @@ use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
 
 /**
  * CreditStrategy
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Hydrator\Strategy
@@ -40,14 +40,14 @@ class CreditStrategy implements StrategyInterface
 {
     /**
      * Instance of ClassMethods hydrator
-     * 
+     *
      * @var ClassMethods
      */
     protected $hydrator;
-    
+
     /**
      * Return an instance of ClassMethods hydrator
-     * 
+     *
      * @return ClassMethods
      */
     protected function getHydrator()
@@ -57,12 +57,12 @@ class CreditStrategy implements StrategyInterface
         }
         return $this->hydrator;
     }
-    
+
     /**
      * Extract the values from an instance of Credit
-     * 
+     *
      * @param Credit $value
-     * 
+     *
      * @return null|array
      */
     public function extract($value)
@@ -72,16 +72,19 @@ class CreditStrategy implements StrategyInterface
         }
         return $this->getHydrator()->extract($value);
     }
-    
+
     /**
      * Hydrate and return an instance of Credit using the supplied array of values
-     * 
+     *
      * @param array $value
-     * 
+     *
      * @return Credit
      */
-    public function hydrate($value)
+    public function hydrate($values)
     {
-        return $this->getHydrator()->hydrate($value, new Credit());
+        if (! is_array($values)) {
+            return null;
+        }
+        return $this->getHydrator()->hydrate($values, new Credit());
     }
 }
--- tests/OpenWeatherMapTest/Hydrator/Strategy/CreditStrategyTest.php
+++ tests/OpenWeatherMapTest/Hydrator/Strategy/CreditStrategyTest.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * CreditStrategyTest.php
- * 
+ *
  * @category   OpenWeatherMapTest
  * @package    OpenWeatherMapTest
  * @subpackage OpenWeatherMapTest\Hydrator\Strategy
  * @author     David White [monkeyphp] <david@monkeyphp.com>
- * 
+ *
  * Copyright (C) 2014  David White
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
@@ -31,7 +31,7 @@ use stdClass;
 
 /**
  * CreditStrategyTest
- * 
+ *
  * @category   OpenWeatherMapTest
  * @package    OpenWeatherMapTest
  * @subpackage OpenWeatherMapTest\Hydrator\Strategy
@@ -41,35 +41,53 @@ class CreditStrategyTest extends PHPUnit_Framework_TestCase
 {
     /**
      * Test that we can extract the values
+     *
+     * @covers \OpenWeatherMap\Hydrator\Strategy\CreditStrategy::extract
      */
     public function testExtract()
     {
         $credit = new Credit();
         $strategy = new CreditStrategy();
-        
+
         $this->assertInternalType('array', $strategy->extract($credit));
     }
-    
+
     /**
      * Test that attampting to extract the values from a non Credit
      * instance will return null
+     *
+     * @covers \OpenWeatherMap\Hydrator\Strategy\CreditStrategy::extract
      */
     public function testExtractWithNonCreditInstance()
     {
         $value = new stdClass();
         $strategy = new CreditStrategy();
-        
+
         $this->assertNull($strategy->extract($value));
     }
-    
+
     /**
      * Test that we can hydrate a Credit instance
+     *
+     * @covers \OpenWeatherMap\Hydrator\Strategy\CreditStrategy::hydrate
      */
     public function testHydrate()
     {
         $values = array();
         $strategy = new CreditStrategy();
-        
+
         $this->assertInstanceOf('\OpenWeatherMap\Entity\Credit', $strategy->hydrate($values));
+    }
+
+    /**
+     * Test that supplying a non array paramter results in null being returned
+     *
+     * @covers \OpenWeatherMap\Hydrator\Strategy\CreditStrategy::hydrate
+     */
+    public function testHydrateReturnsNull()
+    {
+        $strategy = new CreditStrategy();
+
+        $this->assertNull($strategy->hydrate(new stdClass()));
     }
 }
