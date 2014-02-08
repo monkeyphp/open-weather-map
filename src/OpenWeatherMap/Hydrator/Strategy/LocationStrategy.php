897bebb6855d94e02d1d43c82a36437dcc11c95e
David White <david@monkeyphp.com>
2014-02-08 15:41:27 +0000

    Updated tests for LocationStrategy

--- src/OpenWeatherMap/Hydrator/Strategy/LocationStrategy.php
+++ src/OpenWeatherMap/Hydrator/Strategy/LocationStrategy.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * LocationStrategy.php
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Hydrator\Strategy
  * @author     David White [monkeyphp] <david@monkeyphp.com>
- * 
+ *
  * Copyright (C) 2014  David White
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
@@ -30,7 +30,7 @@ use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
 
 /**
  * LocationStrategy
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Hydrator\Strategy
@@ -40,14 +40,14 @@ class LocationStrategy implements StrategyInterface
 {
     /**
      * Instance of ClassMethods hydrator
-     * 
+     *
      * @var ClassMethods
      */
     protected $hydrator;
-    
+
     /**
      * Return an instance of ClassMethods hydrator
-     * 
+     *
      * @return ClassMethods
      */
     protected function getHydrator()
@@ -57,12 +57,12 @@ class LocationStrategy implements StrategyInterface
         }
         return $this->hydrator;
     }
-    
+
     /**
      * Extract the values from the supplied object
-     * 
+     *
      * @param Location $value The object to extract values from
-     * 
+     *
      * @return array|null;
      */
     public function extract($value)
@@ -75,9 +75,9 @@ class LocationStrategy implements StrategyInterface
 
     /**
      * Hydrate and return an instance of Location using the supplied value array
-     * 
+     *
      * @param array $value The values to hydrate the Location instance with
-     * 
+     *
      * @return Location|null
      */
     public function hydrate($value)
@@ -85,17 +85,14 @@ class LocationStrategy implements StrategyInterface
         if (! is_array($value)) {
             return null;
         }
-        
         if (isset($value['location']) && is_array($value['location'])) {
             $value += $value['location'];
             unset($value['location']);
         }
-        
         if (isset($value['geobaseid'])) {
             $value['geobaseId'] = $value['geobaseid'];
             unset($value['geobaseid']);
         }
-        
         return $this->getHydrator()->hydrate($value, new Location());
     }
 }
--- tests/OpenWeatherMapTest/Hydrator/Strategy/LocationStrategyTest.php
+++ tests/OpenWeatherMapTest/Hydrator/Strategy/LocationStrategyTest.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * LocationStrategyTest.php
- * 
+ *
  * @category   OpenWeatherMapTest
  * @package    OpenWeatherMapTest
  * @subpackage OpenWeatherMapTest\Hydrator\Strategy
  * @author     David White [monkeyphp] <david@monkeyphp.com>
- * 
+ *
  * Copyright (C) 2014  David White
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
@@ -31,7 +31,7 @@ use stdClass;
 
 /**
  * LocationStrategyTest
- * 
+ *
  * @category   OpenWeatherMapTest
  * @package    OpenWeatherMapTest
  * @subpackage OpenWeatherMapTest\Hydrator\Strategy
@@ -39,22 +39,33 @@ use stdClass;
  */
 class LocationStrategyTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * Test that we can hydrate an Location instance
+     *
+     * @covers \OpenWeatherMap\Hydrator\Strategy\LocationStrategy::hydrate
+     */
     public function testHydrate()
     {
         $value = array();
         $locationStrategy = new LocationStrategy();
-        
+
         $this->assertInstanceOf('\OpenWeatherMap\Entity\Location', $locationStrategy->hydrate($value));
     }
-    
+
+    /**
+     * @covers \OpenWeatherMap\Hydrator\Strategy\LocationStrategy::hydrate
+     */
     public function testHydrateReturnsNull()
     {
         $value = new stdClass();
         $locationStrategy = new LocationStrategy();
-        
+
         $this->assertNull($locationStrategy->hydrate($value));
     }
-    
+
+    /**
+     * @covers \OpenWeatherMap\Hydrator\Strategy\LocationStrategy::hydrate
+     */
     public function testHydrateGeobaseId()
     {
         $geobaseId = 10;
@@ -63,10 +74,13 @@ class LocationStrategyTest extends PHPUnit_Framework_TestCase
         );
         $locationStrategy = new LocationStrategy();
         $location = $locationStrategy->hydrate($value);
-        
+
         $this->assertEquals($geobaseId, $location->getGeobaseId());
     }
-    
+
+    /**
+     * @covers \OpenWeatherMap\Hydrator\Strategy\LocationStrategy::hydrate
+     */
     public function testHydrateLocation()
     {
         $altitude = 0;
@@ -78,21 +92,27 @@ class LocationStrategyTest extends PHPUnit_Framework_TestCase
         );
         $locationStrategy = new LocationStrategy();
         $entity = $locationStrategy->hydrate($value);
-        
+
         $this->assertEquals($entity->getAltitude(), $altitude);
     }
-    
+
+    /**
+     * @covers \OpenWeatherMap\Hydrator\Strategy\LocationStrategy::extract
+     */
     public function testExtract()
     {
         $location = new Location();
         $location->setAltitude(5);
         $locationStrategy = new LocationStrategy();
-        
+
         $array = $locationStrategy->extract($location);
-        
+
         $this->assertArrayHasKey('altitude', $array);
     }
-    
+
+    /**
+     * @covers \OpenWeatherMap\Hydrator\Strategy\LocationStrategy::extract
+     */
     public function testExtractReturnsNull()
     {
         $locationStrategy = new LocationStrategy();
