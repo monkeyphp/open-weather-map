efba511727f31b5bba6b79bc3ad0a2e914849e07
David White <david@monkeyphp.com>
2014-02-08 19:52:55 +0000

    Update tests for SymbolStrategy

--- src/OpenWeatherMap/Hydrator/Strategy/SymbolStrategy.php
+++ src/OpenWeatherMap/Hydrator/Strategy/SymbolStrategy.php
@@ -1,24 +1,24 @@
 <?php
 /**
  * SymbolStrategy.php
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Hydrator\Strategy
  * @author     David White [monkeyphp] <david@monkeyphp.com>
- * 
+ *
  * Copyright (C) 2014  David White
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see [http://www.gnu.org/licenses/].
  */
@@ -30,7 +30,7 @@ use Zend\Stdlib\Hydrator\Strategy\StrategyInterface;
 
 /**
  * SymbolStrategy
- * 
+ *
  * @category   OpenWeatherMap
  * @package    OpenWeatherMap
  * @subpackage OpenWeatherMap\Hydrator\Strategy
@@ -40,14 +40,14 @@ class SymbolStrategy implements StrategyInterface
 {
     /**
      * Instance of ClassMethods hydrator
-     * 
+     *
      * @var ClassMethods
      */
     protected $hydrator;
-    
+
     /**
      * Return an instance of ClassMethods hydrator
-     * 
+     *
      * @return ClassMethods
      */
     protected function getHydrator()
@@ -57,10 +57,10 @@ class SymbolStrategy implements StrategyInterface
         }
         return $this->hydrator;
     }
-    
+
     /**
      * Return an array of values from the supplied Symbol instance
-     * 
+     *
      * @param Symbol $value
      * @return null|array
      */
@@ -74,12 +74,15 @@ class SymbolStrategy implements StrategyInterface
 
     /**
      * Hydrate an instance of Symbol with the supplied array of values
-     * 
+     *
      * @param array $value
      * @return Symbol
      */
     public function hydrate($value)
     {
+        if (! is_array($value)) {
+            return null;
+        }
         return $this->getHydrator()->hydrate($value, new Symbol());
     }
 }
